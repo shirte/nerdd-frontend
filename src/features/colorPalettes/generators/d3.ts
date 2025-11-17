d1d21cc62c66468c66c4fa58c1e547ea57edd9f7
Steffen Hirte <shirte@users.noreply.github.com>
2025-11-17 13:04:46 +1300

    fix: Create d3 palettes in a function

--- src/features/colorPalettes/generators/d3.ts
+++ src/features/colorPalettes/generators/d3.ts
@@ -1,6 +1,7 @@
 import * as d3 from "d3"
+import type { InterpolatorMap, PaletteGenerator, PaletteMap } from "./types"
 
-const d3Schemes = {
+const d3Schemes: Record<string, readonly string[]> = {
     category10: d3.schemeCategory10,
     accent: d3.schemeAccent,
     dark2: d3.schemeDark2,
@@ -13,7 +14,7 @@ const d3Schemes = {
     tableau10: d3.schemeTableau10,
 }
 
-const d3SequentialInterpolators = {
+const d3SequentialInterpolators: InterpolatorMap = {
     // Sequential single-hue
     blues: d3.interpolateBlues,
     greens: d3.interpolateGreens,
@@ -31,7 +32,6 @@ const d3SequentialInterpolators = {
     cividis: d3.interpolateCividis,
     warm: d3.interpolateWarm,
     cool: d3.interpolateCool,
-    cubehelix: d3.interpolateCubehelix,
 
     // Sequential multi-hue, two-tone
     bugn: d3.interpolateBuGn,
@@ -48,7 +48,7 @@ const d3SequentialInterpolators = {
     ylorrd: d3.interpolateYlOrRd,
 }
 
-const d3DivergingInterpolators = {
+const d3DivergingInterpolators: InterpolatorMap = {
     // Diverging
     brbg: d3.interpolateBrBG,
     prgn: d3.interpolatePRGn,
@@ -61,7 +61,7 @@ const d3DivergingInterpolators = {
     spectral: d3.interpolateSpectral,
 }
 
-const d3Interpolators = {
+const d3Interpolators: InterpolatorMap = {
     ...d3SequentialInterpolators,
     ...d3DivergingInterpolators,
 }
@@ -69,7 +69,7 @@ const d3Interpolators = {
 //
 // convert schemes and interpolators to d3 scales (color palettes)
 //
-const categoricalPalettes = Object.entries(d3Schemes).reduce(
+const categoricalPalettes = Object.entries(d3Schemes).reduce<PaletteMap>(
     (acc, [key, value]) => {
         acc[key] = d3.scaleOrdinal(value)
         return acc
@@ -77,27 +77,26 @@ const categoricalPalettes = Object.entries(d3Schemes).reduce(
     {},
 )
 
-const sequentialPalettes = Object.entries(d3SequentialInterpolators).reduce(
-    (acc, [key, value]) => {
-        acc[key] = d3.scaleSequential(value)
-        return acc
-    },
-    {},
-)
-
-const divergingPalettes = Object.entries(d3DivergingInterpolators).reduce(
-    (acc, [key, value]) => {
-        acc[key] = d3.scaleDiverging(value)
-        return acc
-    },
-    {},
-)
-
-const d3Palettes = {
+const sequentialPalettes = Object.entries(
     d3SequentialInterpolators,
+).reduce<PaletteMap>((acc, [key, value]) => {
+    acc[key] = d3.scaleSequential(value)
+    return acc
+}, {})
+
+const divergingPalettes = Object.entries(
     d3DivergingInterpolators,
-    interpolators: d3Interpolators,
-    d3Schemes,
+).reduce<PaletteMap>((acc, [key, value]) => {
+    acc[key] = d3.scaleDiverging(value)
+    return acc
+}, {})
+
+export const d3PaletteGenerator: PaletteGenerator = () => ({
+    null: d3.scaleOrdinal<string, undefined>().range([undefined]),
+    colors: {},
+    interpolators: {
+        ...d3Interpolators,
+    },
     diverging: {
         ...divergingPalettes,
     },
@@ -107,6 +106,4 @@ const d3Palettes = {
     categorical: {
         ...categoricalPalettes,
     },
-}
-
-export default d3Palettes
+})
