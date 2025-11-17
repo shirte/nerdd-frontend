ce15de26b4d6f2d540c8d498ee67ac7d79d39f77
Steffen Hirte <shirte@users.noreply.github.com>
2025-11-17 13:06:19 +1300

    fix: Move color utils

--- src/features/colorPalettes/generators/util.ts
+++ src/features/colorPalettes/generators/util.ts
@@ -1,15 +1,15 @@
 // convert a decimal value to hex
-export function decimal2hex(decimal) {
+export function decimal2hex(decimal: string | number): string {
     // Ensure that we return a two digit hex value
-    return `0${parseInt(decimal).toString(16)}`.slice(-2)
+    return `0${Number.parseInt(`${decimal}`, 10).toString(16)}`.slice(-2)
 }
 
 // convert a hex value to decimal
-export function hex2decimal(hex) {
-    return parseInt(hex, 16)
+export function hex2decimal(hex: string): number {
+    return Number.parseInt(hex, 16)
 }
 
-export function formatHexValue(color) {
+export function formatHexValue(color: string): string {
     let formattedValue = color.replace(/#/g, "")
 
     if (formattedValue.length === 3) {
@@ -23,9 +23,9 @@ export function formatHexValue(color) {
     return formattedValue
 }
 
-const RGBA_PATTERN = /^rgba?\((\d+),\s*(\d+),\s*(\d+)(?:,\s*([\d\.]+))?\)$/
+const RGBA_PATTERN = /^rgba?\((\d+),\s*(\d+),\s*(\d+)(?:,\s*([\d.]+))?\)$/
 
-export function rgb2hex(rgbArg) {
+export function rgb2hex(rgbArg: string): string {
     // A very ugly regex that parses a string such as 'rgb(191, 0, 46)' and produces an array
     const rgb = rgbArg.match(RGBA_PATTERN)
 
@@ -34,20 +34,20 @@ export function rgb2hex(rgbArg) {
     }
 
     // concatenate the pairs
-    return `${decimal2hex(rgb[1])}${decimal2hex(rgb[2])}${decimal2hex(rgb[3])}`
+    return `#${decimal2hex(rgb[1])}${decimal2hex(rgb[2])}${decimal2hex(rgb[3])}`
 }
 
-export function mix(color1Arg, color2Arg, weight = 50) {
+export function mix(color1Arg: string, color2Arg: string, weight = 50): string {
     let color = "#"
 
-    const color1 = rgb2hex(formatHexValue(color1Arg))
-    const color2 = rgb2hex(formatHexValue(color2Arg))
+    const color1 = formatHexValue(rgb2hex(formatHexValue(color1Arg)))
+    const color2 = formatHexValue(rgb2hex(formatHexValue(color2Arg)))
 
     // loop through each of the 3 hex pairs—red, green, and blue
     for (let i = 0; i <= 5; i += 2) {
         // extract the current pairs
-        const value1 = hex2decimal(color1.substr(i, 2))
-        const value2 = hex2decimal(color2.substr(i, 2))
+        const value1 = hex2decimal(color1.slice(i, i + 2))
+        const value2 = hex2decimal(color2.slice(i, i + 2))
 
         // combine the current pairs from each source color, according to the specified weight
         let combinedValue = decimal2hex(
@@ -62,5 +62,9 @@ export function mix(color1Arg, color2Arg, weight = 50) {
         color += combinedValue // concatenate combinedValue to our new color string
     }
 
-    return color // PROFIT!
+    return color
+}
+
+export function lighten(color: string, weight = 40): string {
+    return mix("#ffffff", color, weight)
 }
